99a281fbd5076a9080d3976ffe766a71dd495618
Akanksha Dabas <akankshadabas10@gmail.com>
2026-07-06 21:11:43 -0500

    Starting stacks

---
+++ 01-basics-and-control-flow/Stack.py
@@ -0,0 +1,9 @@
+# Stacks
+# LIFO - last in firstout - the last book you put on the stack is the first one you can take out.
+browsing_session = []
+browsing_session.append(1)
+browsing_session.append(2)
+browsing_session.append(3)
+print(browsing_session)
+# when the user presses back the last page should be removed from the stack
+browsing_session.pop()
