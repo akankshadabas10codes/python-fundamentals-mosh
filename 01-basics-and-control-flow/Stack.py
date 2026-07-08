ff4594004797079d7f617ffdd9d7054a4101e2a9
Akanksha Dabas <akankshadabas10@gmail.com>
2026-07-08 12:22:41 -0500

    Stacks

--- 01-basics-and-control-flow/Stack.py
+++ 01-basics-and-control-flow/Stack.py
@@ -6,4 +6,15 @@ browsing_session.append(2)
 browsing_session.append(3)
 print(browsing_session)
 # when the user presses back the last page should be removed from the stack
-browsing_session.pop()
+last = browsing_session.pop()
+print(last)
+print(browsing_session)
+print("redirect", browsing_session[-1])
+# if the stack is empty, we should disable the back button
+# we do if not browsing_session to check if the stack is empty,
+# because an empty list is considered false in python, therefore
+# if the list is empty, the not operator will return true and the
+# code block will be executed.
+if not browsing_session:
+    browsing_session[-1]
+    print("disable back button")
