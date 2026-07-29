be1625a2a528947ad987a1bd88be1fe9bf89c745
Akanksha Dabas <akankshadabas10@gmail.com>
2026-07-29 12:09:13 -0500

    properties continued

--- 01-basics-and-control-flow/.vscode/ClassesCont.py
+++ 01-basics-and-control-flow/.vscode/ClassesCont.py
@@ -113,10 +113,18 @@ print(cloud.__dict__)
 
 class Product:
     def __init__(self, price):
-        self.__price = price
+        self.price = price
 
-    def gert_price(self):
+    @property
+    def price(self):
         return self.__price
 
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price cannot be negative.")
+        self.__price = value
 
-product = Product(-50)
+
+product = Product(10)
+print(product.price)
